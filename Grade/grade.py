0e4910342ffda2f720e9241acd2fea391069d28a
Thi <mthi12347@gmail.com>
2025-12-05 19:59:11 +0700

    Add a command for Grade

--- Grade/grade.py
+++ Grade/grade.py
@@ -3,6 +3,7 @@ class Grade:
         self.student = student
         self.course = course
         self.score = score
+        # xyz
 
     def show_grade(self):
         print(f"Grade: {self.student.name} - {self.course.course_name} = {self.score}")
